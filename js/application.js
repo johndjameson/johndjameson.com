493504da5bcb0a059a78b46724601ae566962248
John D. Jameson <john@San-Antonio.local>
2013-11-13 22:28:09 -0500

    Tweaked blockHeader and Disqus, started documentation

--- js/application.js
+++ js/application.js
@@ -1,49 +1,53 @@
-var $window = $(window),
+(function() {
+    var $window = $(window),
     $document = $(document),
-    // $html = $('html'),
     $post = $('.post'),
     $sups = $('.post sup'),
     $footnotes = $('.post-footnotes');
-    
-// var commentsExist = false;
 
-function createSidenotes() {
-  var $footnoteArray = $footnotes.children();
+    /*  Developer Tools
+    ==================================================*/
 
-  $sups.parent().wrap("<div class='post-subject'></div>");
+    function devResize() {
+        $('#dev_log').html($window.width() + "px, " + $window.width()/16 + "em");
+    }
+        
+    function createSidenotes() {
+        var $footnoteArray = $footnotes.children();
 
-  for (var i = 0; i < $sups.length; i++) {
-    $($('.post-subject')[i]).append("<aside class='post-sidenote'><p>" + $($footnoteArray[i]).html() + "</p></aside>");
-    $('.post-sidenote').siblings().css({
-      marginBottom: 0
+        $sups.parent().wrap("<div class='post-subject'></div>");
+
+        for (var i = 0; i < $sups.length; i++) {
+            $($('.post-subject')[i]).append("<aside class='post-sidenote'><p>" + $($footnoteArray[i]).html() + "</p></aside>");
+            $('.post-sidenote').siblings().css({
+                marginBottom: 0
+            });
+        }
+    }
+
+    function toggleNotes() {
+        if ($footnotes.length > 0 && $sups.length > 0) {
+        $post.addClass('has-sidenotes');
+        }
+    }
+
+    // this works because div#disqus_thread is only on post pages
+    function createComments() {
+        var disqus_shortname = 'johndjameson',
+            dsq = document.createElement('script');
+            dsq.type = 'text/javascript'; dsq.async = true;
+            dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+
+        (document.getElementsByTagName('body')[0]).appendChild(dsq);
+    }
+
+    $document.ready(function() {
+        createSidenotes();
+        createComments();
+        toggleNotes();
     });
-  }
-}
 
-function toggleNotes() {
-  $('#dev_log').html($window.width() + "px, " + $window.width()/16 + "em");
-
-  if ($footnotes.length > 0 && $sups.length > 0) {
-    $post.addClass('has-sidenotes');
-  }
-}
-
-$document.ready(function() {
-  createSidenotes();
-  toggleNotes();
-});
-
-$window.resize(function() {
-  toggleNotes();
-});
-
-// $(window).scroll(function() {
-//   if (!commentsExist) {
-//    if($(window).scrollTop() + $(window).height() > $(document).height() - 400) {
-//      commentsExist = true;
-
-//      $('article.post').append('<div id="disqus_thread"></div>');
-//      $('body').append('<script async src="/js/disqus.js"></script>');
-//    }
-//  }
-// });
+    $window.resize(function() {
+        devResize();
+    });
+}());
