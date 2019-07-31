93603774bae4f7ad04268759e34614aca29e6930
John D. Jameson <thejohnjameson@gmail.com>
2019-07-31 00:25:10 -0500

    Fix markup errors

--- src/components/header.js
+++ src/components/header.js
@@ -10,12 +10,12 @@ const Header = () => (
         <div className='mv-g aife'>
           <div className='mv-g-b mv-g-b--3of5 mv-g-b--4of5_m'>
             <div className='aife df'>
-              <Link className='db mrm' href='/'>
-                <img className='db logo' src={logo} alt='' />
+              <Link className='db mrm' to='/'>
+                <img alt='' className='db logo' src={logo} />
               </Link>
-              <div class='dn db_m'>
-                <p class='mbf tss tw7'>John D. Jameson</p>
-                <p class='mbf tsi tss'>Blog posts on web typography</p>
+              <div className='dn db_m'>
+                <p className='mbf tss tw7'>John D. Jameson</p>
+                <p className='mbf tsi tss'>Blog posts on web typography</p>
               </div>
             </div>
           </div>
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -13,23 +13,23 @@ function PageTemplate({ data: { mdx: post } }) {
         title={post.frontmatter.title}
       />
 
-      <article class='row'>
-        <section class='cell well well--l'>
-          <div class='mv-g mv-g--centered_l'>
-            <div class='mv-g-b mv-g-b--4of5_m mv-g-b--5of5_l'>
-              <h1 class='title tac_l'>{post.frontmatter.title}</h1>
+      <article className='row'>
+        <section className='cell well well--l'>
+          <div className='mv-g mv-g--centered_l'>
+            <div className='mv-g-b mv-g-b--4of5_m mv-g-b--5of5_l'>
+              <h1 className='title tac_l'>{post.frontmatter.title}</h1>
             </div>
           </div>
 
-          <div class='mv-g mv-g--centered_l'>
-            <div class='mv-g-b mv-g-b--4of5_m mv-g-b--3of5_l'>
-              <div class='mbm mbl_l tw7'>{post.frontmatter.description}</div>
+          <div className='mv-g mv-g--centered_l'>
+            <div className='mv-g-b mv-g-b--4of5_m mv-g-b--3of5_l'>
+              <div className='mbm mbl_l tw7'>{post.frontmatter.description}</div>
             </div>
           </div>
 
-          <div class='article'>
-            <div class='mv-g mv-g--centered_l'>
-              <div class='mv-g-b mv-g-b--4of5_m mv-g-b--3of5_l'>
+          <div className='article'>
+            <div className='mv-g mv-g--centered_l'>
+              <div className='mv-g-b mv-g-b--4of5_m mv-g-b--3of5_l'>
                 <MDXRenderer>{post.body}</MDXRenderer>
               </div>
             </div>
