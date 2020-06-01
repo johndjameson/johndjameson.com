9505a07536c180771d721e1bec2ec7999d1c3b43
John D. Jameson <thejohnjameson@gmail.com>
2020-05-31 20:55:37 -0500

    Burn it all with fire

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,7 @@
 import React from 'react'
 
-import Layout from 'components/layout'
-import SEO from 'components/seo'
-
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title='404: Not found' />
-    <h1>Not Found</h1>
-    <p>You just hit a route that doesn’t exist… the sadness.</p>
-  </Layout>
+export default () => (
+  <div>
+    <h1>404 - Oh no's! We couldn't find that page :(</h1>
+  </div>
 )
-
-export default NotFoundPage
