3404991c903413a36787df07102eeeaf8235bdca
John D. Jameson <thejohnjameson@gmail.com>
2020-07-11 16:52:03 -0500

    Add hero responsive styles

--- src/components/Hero/styled.js
+++ src/components/Hero/styled.js
@@ -1,59 +1,84 @@
 import styled, { createGlobalStyle } from 'styled-components'
 import { rem, rgba } from 'polished'
+import { media } from 'styles/mixins'
 import Polaroid from 'components/Polaroid'
 
-const bgColor = '#161616'
+const heroBackground = '#161616'
+
+const heroBreakpoints = {
+  layout: 800,
+}
 
 export const HeroGlobal = createGlobalStyle`
   html {
-    background-color: ${bgColor};
+    background-color: ${heroBackground};
   }
 `
 
 export const HeroBase = styled.main`
   --hero-image-overlap: 90px;
-  background-color: ${bgColor};
+  --hero-padding: 20px;
+  align-items: center;
+  background-color: ${heroBackground};
   color: #fffefa;
   display: flex;
-  font-size: ${rem(21)};
+  font-size: ${rem(18)};
   justify-content: center;
   line-height: 1.5;
   min-height: 100vh;
-  padding-left: 20px;
-  padding-right: 20px;
+  padding-left: var(--hero-padding);
+  padding-right: var(--hero-padding);
+
+  ${media.widerThan(heroBreakpoints.layout)`
+    --hero-padding: 40px;
+    font-size: ${rem(21)};
+  `}
 `
 
 export const HeroGrid = styled.div`
-  align-items: center;
   display: grid;
   flex-grow: 1;
-  grid-column-gap: 30px;
-  grid-template-columns: 1fr 1fr;
+  justify-content: center;
   max-width: 1280px;
+
+  ${media.widerThan(heroBreakpoints.layout)`
+    align-items: center;
+    grid-column-gap: 60px;
+    grid-template-columns: 1fr 1fr;
+  `}
 `
 
 export const HeroImages = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
-  padding-left: 1%; // Magic number to indent rotation
-  padding-right: 3%; // Magic number to indent rotation
+  padding-left: 1.5%; // Magic number to indent rotation
+  padding-right: 3.5%; // Magic number to indent rotation
+
+  ${media.narrowerThan(heroBreakpoints.layout)`
+    margin-bottom: 40px;
+    margin-left: auto;
+    margin-right: auto;
+    max-width: 400px;
+    padding-top: 40px;
+  `}
 `
 
 const HeroImage = styled(Polaroid)`
-  box-shadow: 0 3px 10px ${rgba(bgColor, 0.5)};
+  box-shadow: 0 3px 10px ${rgba(heroBackground, 0.5)};
+  max-width: none;
   position: relative;
   transition: transform 0.2s ease-in;
-  width: 100%;
+  width: calc(100% + (var(--hero-image-overlap) * 0.5));
 
   &:hover {
-    box-shadow: 0 4px 20px ${rgba(bgColor, 0.4)};
+    box-shadow: 0 4px 20px ${rgba(heroBackground, 0.4)};
     transform: scale(1.05);
     transition-timing-function: ease-out;
   }
 `
 
 export const HeroSummer = styled(HeroImage)`
-  left: calc(var(--hero-image-overlap) * 0.5);
+  // left: calc(var(--hero-image-overlap) * 0.5);
   transform: rotate(-2deg);
   z-index: 1;
 `
@@ -73,9 +98,13 @@ export const HeroContent = styled.div`
 
 export const HeroHeading = styled.h1`
   font-family: var(--font-display);
-  font-size: ${rem(92)};
+  font-size: ${rem(60)};
   font-style: italic;
   font-weight: 200;
   line-height: 0.85;
-  margin-bottom: 20px;
+
+  ${media.widerThan(heroBreakpoints.layout)`
+    margin-bottom: 20px;
+    font-size: ${rem(92)};
+  `}
 `
