958a69519102fbbee09c5b872da06af6f6d890b3
John D. Jameson <thejohnjameson@gmail.com>
2022-05-28 14:22:13 -0500

    Add Container component

---
+++ components/Container/Container.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  padding-left: var(--jdj-layout-container-width-padding-x);
+  padding-right: var(--jdj-layout-container-width-padding-x);
+`;
+
+export default Container;
