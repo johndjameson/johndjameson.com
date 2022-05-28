bafb64c361512d808c2cf601652bfce43efef7e9
John D. Jameson <thejohnjameson@gmail.com>
2022-05-28 18:13:07 -0500

    Add Container widths

--- components/Container/Container.jsx
+++ components/Container/Container.jsx
@@ -1,8 +1,24 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const Container = styled.div`
-  padding-left: var(--jdj-layout-container-width-padding-x);
-  padding-right: var(--jdj-layout-container-width-padding-x);
+  ${({ width }) => {
+    switch (width) {
+      case 'narrow':
+        return css`
+          padding-left: var(--jdj-layout-container-width-narrow-padding-x);
+          padding-right: var(--jdj-layout-container-width-narrow-padding-x);
+        `;
+      case 'default':
+        return css`
+          padding-left: var(--jdj-layout-container-width-padding-x);
+          padding-right: var(--jdj-layout-container-width-padding-x);
+        `;
+    }
+  }}
 `;
+
+Container.defaultProps = {
+  width: 'default',
+};
 
 export default Container;
