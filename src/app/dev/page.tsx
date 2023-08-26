ef5bad518e8576dad6a379bc2bbd55ba228050d5
John D. Jameson <thejohnjameson@gmail.com>
2023-08-26 18:13:43 -0500

    Update Cendra fallbacks and custom prop

--- src/app/dev/page.tsx
+++ src/app/dev/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "next";
+import devStyles from "@/app/dev/dev.module.css";
 
 export const metadata: Metadata = {
   title: "Development",
@@ -7,6 +8,8 @@ export const metadata: Metadata = {
   },
 };
 
+const overlapText = "Styling Fallback Fonts with Sass";
+
 export default function Home() {
   return (
     <div
@@ -14,22 +17,29 @@ export default function Home() {
         padding: "var(--space-200) var(--layout-container-width-padding-x) 0",
       }}
     >
-      {[100, 300, 400, 500, 600, 700, 800, 900].map((weight) =>
-        [false, true].map((italic) => (
-          <p
-            key={`${weight}-${italic}`}
-            style={{
-              fontFamily: "cendra-web",
-              fontSize: 40,
-              fontStyle: italic ? "italic" : "normal",
-              fontWeight: weight,
-              lineHeight: 1.2,
-            }}
-          >
-            Example Text
-          </p>
-        ))
-      )}
+      <>
+        <div className={devStyles.overlap}>
+          <p className={devStyles.fallback}>{overlapText}</p>
+          <p className={devStyles.primary}>{overlapText}</p>
+        </div>
+
+        {[100, 300, 400, 500, 600, 700, 800, 900].map((weight) =>
+          [false, true].map((italic) => (
+            <p
+              key={`${weight}-${italic}`}
+              style={{
+                fontFamily: "cendra-web",
+                fontSize: 40,
+                fontStyle: italic ? "italic" : "normal",
+                fontWeight: weight,
+                lineHeight: 1.2,
+              }}
+            >
+              Example Text
+            </p>
+          ))
+        )}
+      </>
     </div>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,16 @@
-import "@/app/layers.css";
-import "@/app/modern-normalize-layer.css";
 import "@/app/font-cendra.css";
 import "@/app/globals.css";
+import "@/app/layers.css";
+import "@/app/modern-normalize-layer.css";
+import { Inter } from "next/font/google";
 import Header from "@/components/Header/Header";
 import type { Metadata } from "next";
+
+const inter = Inter({
+  display: "swap",
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
 
 export const metadata: Metadata = {
   title: "John D. Jameson | Front-End Engineer",
@@ -16,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en-US">
+    <html lang="en-US" className={inter.variable}>
       <body>
         <Header />
         {children}
