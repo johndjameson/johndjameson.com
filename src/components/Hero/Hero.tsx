3e217c9cad730a7803bf807654da43a38f406ebb
John D. Jameson <thejohnjameson@gmail.com>
2023-08-27 13:52:26 -0500

    Update hero styles

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -19,15 +19,20 @@ function Hero({ children }: HeroProps) {
   return (
     <div aria-hidden="true" className={heroStyles.container}>
       <div className={heroStyles.content}>
-        {Array.from({ length: 16 }).map((_value, index) => (
-          <p
-            className={heroStyles.line}
-            key={index}
-            style={{ translate: `${index * 3}%` }}
-          >
-            {memoString}
-          </p>
-        ))}
+        <div className={heroStyles.lines}>
+          {Array.from({ length: 16 }).map((_value, index) => (
+            <p
+              className={heroStyles.line}
+              key={index}
+              style={{
+                fontSize: `${(index % 2 ? 4 : 8) + Math.random() * 3}cqw`,
+                translate: `${index * 3}%`,
+              }}
+            >
+              {memoString}
+            </p>
+          ))}
+        </div>
       </div>
     </div>
   );
