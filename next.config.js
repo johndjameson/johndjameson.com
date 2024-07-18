5393f0add42068c79a1f7f422a9856df3563c7f7
John D. Jameson <thejohnjameson@gmail.com>
2024-07-18 18:04:45 -0500

    Update header source syntax

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/(.*)",
+        source: "/:path*",
         headers: [
           { key: "Link", value: '<https://jdj.land>; rel="preconnect"' },
         ],
