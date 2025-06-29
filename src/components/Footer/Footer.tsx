3db4c3050e665fbab86718ed300ac390001e7599
John D. Jameson <thejohnjameson@gmail.com>
2025-06-29 12:08:56 -0500

    Update footer container

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,55 +3,53 @@ import { FaGithub, FaLinkedin, FaXTwitter } from "react-icons/fa6";
 
 export default function Footer() {
   return (
-    <footer className="border-t border-slate-300 bg-slate-100">
-      <div className="px-container-w-narrow py-12">
-        <div className="flex flex-col space-y-6 md:items-center">
-          <div className="mb-6 text-pretty md:text-center">
-            <p className="mb-2 text-slate-700">
-              John D. Jameson is a front-end engineer in Austin, TX, with 12+
-              years of hands-on experience building web applications at
-              companies ranging from startups to Amazon.
-            </p>
+    <footer className="px-container-w-narrow border-t border-slate-300 bg-slate-100 py-12">
+      <div className="flex flex-col space-y-6 md:items-center">
+        <div className="mb-6 text-pretty md:text-center">
+          <p className="mb-2 text-slate-700">
+            John D. Jameson is a front-end engineer in Austin, TX, with 12+
+            years of hands-on experience building web applications at companies
+            ranging from startups to Amazon.
+          </p>
 
-            <p className="text-slate-700">
-              Want to reach out? Let’s connect on{" "}
-              <DynamicLink
-                className="font-medium text-slate-700 underline transition-colors hover:text-slate-900"
-                href="https://www.linkedin.com/in/johndjameson"
-              >
-                LinkedIn
-              </DynamicLink>
-              .
-            </p>
-          </div>
-
-          <div className="flex space-x-6">
+          <p className="text-slate-700">
+            Want to reach out? Let’s connect on{" "}
             <DynamicLink
-              className="text-slate-700 transition-colors hover:text-slate-900"
+              className="font-medium text-slate-700 underline transition-colors hover:text-slate-900"
               href="https://www.linkedin.com/in/johndjameson"
             >
-              <FaLinkedin size={24} />
+              LinkedIn
             </DynamicLink>
+            .
+          </p>
+        </div>
 
-            <a
-              href="https://github.com/johndjameson"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-slate-700 transition-colors hover:text-slate-900"
-              aria-label="GitHub"
-            >
-              <FaGithub size={24} />
-            </a>
-            <a
-              href="https://x.com/johndjameson"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-slate-700 transition-colors hover:text-slate-900"
-              aria-label="X (Twitter)"
-            >
-              <FaXTwitter size={24} />
-            </a>
-          </div>
+        <div className="flex space-x-6">
+          <DynamicLink
+            className="text-slate-700 transition-colors hover:text-slate-900"
+            href="https://www.linkedin.com/in/johndjameson"
+          >
+            <FaLinkedin size={24} />
+          </DynamicLink>
+
+          <a
+            href="https://github.com/johndjameson"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-slate-700 transition-colors hover:text-slate-900"
+            aria-label="GitHub"
+          >
+            <FaGithub size={24} />
+          </a>
+          <a
+            href="https://x.com/johndjameson"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-slate-700 transition-colors hover:text-slate-900"
+            aria-label="X (Twitter)"
+          >
+            <FaXTwitter size={24} />
+          </a>
         </div>
       </div>
     </footer>
