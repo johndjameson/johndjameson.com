f8c332f5567eb6ca56a86ecbcad611f8a5a84578
John D. Jameson <thejohnjameson@gmail.com>
2025-07-06 14:41:06 -0500

    Squashed hero branch
    commit 412bea3119a9d5799d8a39a6912f8e1cfd697eb2
    Author: John D. Jameson <thejohnjameson@gmail.com>
    Date:   Sun Jul 6 14:40:40 2025 -0500
    
        Restore hero .svg
    
    commit 06d10ef02f12f5e6186763b4eeaaab9fa89483e3
    Author: John D. Jameson <thejohnjameson@gmail.com>
    Date:   Sun Jul 6 14:19:02 2025 -0500
    
        Add hero SVG inline, hover effect
    
    commit 77ee4ab0938147e5359d04aa7b8ac627b14c75b7
    Author: John D. Jameson <thejohnjameson@gmail.com>
    Date:   Sun Jul 6 13:40:45 2025 -0500
    
        Add motion to hero

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,51 @@
+import { clsx } from "clsx";
+
+interface HeroProps {
+  className?: string;
+}
+
+export const Hero = ({ className }: HeroProps) => {
+  return (
+    <div className={className}>
+      <svg className="sr-only" aria-hidden="true">
+        <defs>
+          <filter id="hero-wiggle">
+            <feTurbulence
+              baseFrequency={0.01}
+              numOctaves={1}
+              type="fractalNoise"
+              result="turbulence"
+            >
+              <animate
+                attributeName="baseFrequency"
+                dur="20s"
+                repeatCount="indefinite"
+                values="0.01; 0.02; 0.01"
+              />
+            </feTurbulence>
+            <feDisplacementMap
+              in="SourceGraphic"
+              in2="turbulence"
+              scale="5"
+              xChannelSelector="R"
+              yChannelSelector="G"
+              result="displace"
+            />
+            <feBlend in="displace" in2="SourceGraphic" />
+          </filter>
+        </defs>
+      </svg>
+
+      <img
+        alt="Alternating lines of the words “John D. Jameson Front-End Engineer”"
+        className={clsx(
+          "bg-[#2b206d]",
+          "motion-safe:filter-[url('#hero-wiggle')] motion-safe:will-change-[filter]",
+        )}
+        height={600}
+        width={1280}
+        src="/images/hero.svg"
+      />
+    </div>
+  );
+};
