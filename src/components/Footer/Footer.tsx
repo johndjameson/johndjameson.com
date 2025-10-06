f742fc1b3e509a43b46d7b7a51f4aed2f2ad1e3d
John D. Jameson <thejohnjameson@gmail.com>
2025-10-06 13:26:25 -0500

    Update footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,26 +7,25 @@ export default function Footer() {
     <footer className={clsx("px-container-w bg-neutral-950 py-12", "md:py-16")}>
       <div className="grid gap-x-4 border-2 border-neutral-50 bg-neutral-50 bg-clip-content p-1 text-neutral-950 md:grid-cols-6">
         <div className="flex flex-col px-4 py-10 md:col-span-4 md:col-start-2 md:px-0">
-          <p className="mb-8 text-pretty sm:text-lg md:text-xl">
+          <p className="mb-8 text-sm text-pretty sm:text-lg md:text-xl">
             <strong className="font-bold">John D. Jameson</strong> is a
             front-end engineer in Austin, TX, with 12+ years of hands-on
             experience building web applications at companies ranging from
             startups to Amazon.
           </p>
 
-          <p className="mb-12 border-2 border-black p-6 text-center md:text-lg">
+          <p className="mb-12 border-2 border-black p-6 text-center text-sm sm:text-base md:text-lg">
             Want to reach out? Let’s connect on{" "}
             <DynamicLink
-              className="group/link inline-flex gap-x-1 font-medium underline underline-offset-3 transition-colors"
+              className="group/link inline-flex items-center gap-x-1 font-medium underline underline-offset-3 transition-colors"
               href="https://www.linkedin.com/in/johndjameson"
             >
               <FaLinkedin
-                className="self-center transition group-hover/link:scale-110"
+                className="relative transition group-hover/link:scale-110 max-sm:-top-[0.2em]"
                 size={24}
               />{" "}
               <span className="self-baseline">LinkedIn</span>
             </DynamicLink>
-            .
           </p>
 
           <div className="flex space-x-6">
